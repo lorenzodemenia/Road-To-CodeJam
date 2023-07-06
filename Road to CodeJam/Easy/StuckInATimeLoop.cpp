e781084b465b96d5c669a2efae9e4938bfcb64c4
Lorenzo Demenia <71456537+lorenzodemenia@users.noreply.github.com>
2023-07-06 11:08:42 +0200

    Add files via upload

---
+++ Road to CodeJam/Easy/StuckInATimeLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+
+    for (size_t i = 0; i < num; i++)
+    {
+        cout<<i+1<<" Abracadabra"<<endl;
+    }
+    
+
+    return 0;
+}
---
+++ Road to CodeJam/Easy/TakeTwoStones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N; 
+    cin>>N;
+
+    if(N%2!=0){
+        cout<<"Alice"<<endl;
+    }else{
+        cout<<"Bob"<<endl;
+    }
+
+    return 0;
+}
